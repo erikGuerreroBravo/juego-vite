5c6584eb3f5b23bd550edff5049759df0e4f3c8d
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-19 01:16:51 -0600

    feat:se agrega  el archivo pedir-carta.js con la funcionalidad correspondiente al metodo solicitado pedir-carta

---
+++ proyect-juego/src/blackjack/usecase/pedir-carta.js
@@ -0,0 +1,16 @@
+
+/**
+ * Esta función permite tomar una carta del deck
+ * @param {array<String>} _deck 
+ * @returns  {String} retorna la carta del deck
+ */
+export const pedirCarta = (deck) => {
+
+    if (!deck  || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
+
